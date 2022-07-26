c5d4cf976bba3bc631d7c05815c25404bc735227
joscastro-ing <104741377+joscastro-ing@users.noreply.github.com>
2022-07-26 10:49:40 -0600

    upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useState} from "react";
 import Formulario from "./componentes/Formularios";
+import Cita from "./componentes/Cita";
 
 function App() {
+  //Arreglo de citas
+  const [citas, guardarCitas] = useState([]);
+
+  const crearCita = cita =>{
+    guardarCitas([
+      ...citas, cita
+    ])
+  }  
+
+  //función eliminar cita
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id)
+    guardarCitas(nuevasCitas)
+  }
+//  Cuando mo hay citas mostrar agregar nueva Cita
+const titulo = citas.length ===0? 'No hay Citas' : 'Administrar tus citas'
+
   return (
     <Fragment>
        <div className="App">
@@ -10,9 +28,19 @@ function App() {
       <div className="container">
       <div className="row">
         <div className="one-half column">
-          <Formulario/>
+          <Formulario
+          crearCita={crearCita}
+          />
         </div>
         <div className="one-half column">
+          <h2>{titulo}</h2>
+          {citas.map(cita => (
+            <Cita 
+            key = {cita.id}
+            cita = {cita}
+            eliminarCita = {eliminarCita}
+            />
+          ))}
         </div>
         
       </div>
